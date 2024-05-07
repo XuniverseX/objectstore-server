ea64567c1cf3ab1d595eec9f9dfcd12d5887fbbe
XuniverseX <1029805801@qq.com>
2024-05-07 22:08:20 +0800

    除了docker部署功能，全部完成

---
+++ config/minio.go
@@ -0,0 +1,8 @@
+package config
+
+const (
+	MinioEndpoint        = "127.0.0.1:9000"
+	MinioAccessKeyID     = "JCCpZPa47fJJ52EbzpTw"
+	MinioSecretAccessKey = "3fONzMXFM5Gd543nftNgIp314Ee56Km5nx076J49"
+	UseSSL               = false
+)
